c50c42813c1d2d585fef7a9809fe87bc99367260
= <=>
2022-01-03 03:24:27 -0300

    Aula - 3.9. Configurando a geração de identificador com @TableGenerator

--- wfm-algaworks-ecomerce/src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ wfm-algaworks-ecomerce/src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -30,9 +31,25 @@ public class Categoria {
 //    @EqualsAndHashCode.Include
 //    private Integer id;
     
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq") //mysql não tem sequence mas o hibernate faz uma adaptacão com uma tabela com nome padrão hibernate_sequence
+//    @SequenceGenerator(name = "seq", sequenceName = "categoria_sequencia_chave_primaria", allocationSize = 50) //utiliza uma tabela só para categoria com o nome categoria_sequencia_chave_primaria //utiliza a sequence 50 vezes em memória para então voltar na tabela, padrão é 50
+//    @EqualsAndHashCode.Include
+//    private Integer id;
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.TABLE) //deixando assim já funciona
+//    @EqualsAndHashCode.Include
+//    private Integer id;
+	
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq") //mysql não tem sequence mas o hibernate faz uma adaptacão com uma tabela com nome padrão hibernate_sequence
-    @SequenceGenerator(name = "seq", sequenceName = "categoria_sequencia_chave_primaria", allocationSize = 50) //utiliza uma tabela só para categoria com o nome categoria_sequencia_chave_primaria //utiliza a sequence 50 vezes em memória para então voltar na tabela, padrão é 50
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tabela") //
+    @TableGenerator(name = "tabela", table = "hibernate_sequences", 
+    					pkColumnName = "sequence_name", 
+    					pkColumnValue = "categoria", 
+    					valueColumnName = "next_val", 
+    					initialValue = 0, 
+    					allocationSize = 50)
     @EqualsAndHashCode.Include
     private Integer id;
 
