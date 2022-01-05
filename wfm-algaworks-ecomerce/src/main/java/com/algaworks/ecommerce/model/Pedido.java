6a384d6019e1e0466f87f86c87c8ffc30c0e08e6
William Felipe Marx <marxwilliamf@gmail.com>
2022-01-05 03:10:31 -0300

    Aula - 4.5. Exercício: mapeando relacionamentos um-para-muitos

--- wfm-algaworks-ecomerce/src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ wfm-algaworks-ecomerce/src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -2,6 +2,7 @@ package com.algaworks.ecommerce.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -13,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.EqualsAndHashCode;
@@ -43,6 +45,9 @@ public class Pedido {
     @ManyToOne
     @JoinColumn(name = "cliente_id") //quem tem o @JoinColumn é o Owner
     private Cliente cliente;
+    
+    @OneToMany(mappedBy = "pedido")
+    List<ItemPedido> itens;
     
     @Column(name = "data_pedido")
     private LocalDateTime dataPedido;
--- wfm-algaworks-ecomerce/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoOneToManyTest.java
+++ wfm-algaworks-ecomerce/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoOneToManyTest.java
@@ -39,5 +39,89 @@ public class RelacionamentoOneToManyTest extends EntityManagerTest {
 	}
 	
 	
+	@Test
+	public void verificaRelacionamentoPedido() {
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+		Produto produto1 = entityManager.find(Produto.class, 1);
+		Produto produto2 = entityManager.find(Produto.class, 3);
+		
+		Pedido pedido = new Pedido();
+		pedido.setStatus(StatusPedido.AGUARDANDO);
+		pedido.setDataPedido(LocalDateTime.now());
+		pedido.setTotal(new BigDecimal(1998));
+		
+		pedido.setCliente(cliente);
+				
+		ItemPedido itemPedido1 = new ItemPedido();
+		itemPedido1.setPedido(pedido);
+		itemPedido1.setProduto(produto1);
+		itemPedido1.setPrecoProduto(new BigDecimal(499));
+		itemPedido1.setQuantidade(1);
+
+		ItemPedido itemPedido2 = new ItemPedido();
+		itemPedido2.setPedido(pedido);
+		itemPedido2.setProduto(produto2);
+		itemPedido2.setPrecoProduto(new BigDecimal(1499));
+		itemPedido2.setQuantidade(1);
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(itemPedido1);
+		entityManager.persist(itemPedido2);
+		entityManager.persist(pedido); //Se for na mesma transação posso colocar pedido depois que o hibernate corrige
+		entityManager.getTransaction().commit();
+		
+		//--------------------------------------
+		
+//		entityManager.getTransaction().begin();
+//		entityManager.persist(pedido);					//Caso for em transação diferente preciso persistir primeiro pedido, caso contrário da erro
+//		entityManager.getTransaction().commit();
+//		
+//		entityManager.getTransaction().begin();
+//		entityManager.persist(itemPedido1);
+//		entityManager.persist(itemPedido2);
+//		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+		Assertions.assertFalse(pedidoVerificacao.getItens().isEmpty());
+		
+		System.out.println("---------------------------------------------\nCompra:");
+		for(ItemPedido itemPedido:pedidoVerificacao.getItens()) {
+			System.out.println("Produto " + itemPedido.getProduto().getNome() + " valor: " + itemPedido.getPrecoProduto().toString());			
+		}
+		System.out.println("\t\t\tTotal: " + pedido.getTotal());
+
+	}
 	
+	@Test
+	public void verificaRelacionamentoUtemPedidoProfessor() {
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+		Produto produto = entityManager.find(Produto.class, 1);
+		
+		Pedido pedido = new Pedido();
+		pedido.setStatus(StatusPedido.AGUARDANDO);
+		pedido.setDataPedido(LocalDateTime.now());
+		pedido.setTotal(BigDecimal.TEN);
+		
+		pedido.setCliente(cliente);
+				
+		ItemPedido itemPedido = new ItemPedido();
+		itemPedido.setPrecoProduto(produto.getPreco());
+		itemPedido.setQuantidade(1);
+		itemPedido.setPedido(pedido);
+		itemPedido.setProduto(produto);
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(pedido);
+		entityManager.persist(itemPedido);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+		Assertions.assertFalse(pedidoVerificacao.getItens().isEmpty());
+		
+		System.out.println(pedidoVerificacao.getItens().get(0).getProduto().getNome());
+	}
 }
