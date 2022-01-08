50e6b43e1ea0107bcc26f79aa17c5cd32b11f326
William Felipe Marx <marxwilliamf@gmail.com>
2022-01-07 23:32:45 -0300

    Aula - 4.10. Exercício: mapeando relacionamentos um-para-um

---
+++ wfm-algaworks-ecomerce/comandos.sql
@@ -0,0 +1,23 @@
+SELECT * FROM `wfm-ecommerce`.categoria;
+SELECT * FROM `wfm-ecommerce`.cliente;
+SELECT * FROM `wfm-ecommerce`.etoque;
+SELECT * FROM `wfm-ecommerce`.item_pedido;
+SELECT * FROM `wfm-ecommerce`.nota_fiscal;
+SELECT * FROM `wfm-ecommerce`.pagamento;
+SELECT * FROM `wfm-ecommerce`.pedido;
+SELECT * FROM `wfm-ecommerce`.produto;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- wfm-algaworks-ecomerce/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoOneToOneTest.java
+++ wfm-algaworks-ecomerce/src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentoOneToOneTest.java
@@ -3,6 +3,7 @@ package com.algaworks.ecommerce.relacionamentos;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Date;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,38 +17,62 @@ import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.model.StatusPagamento;
 import com.algaworks.ecommerce.model.StatusPedido;
+import com.algaworks.ecommerce.model.NotaFiscal;
 
 public class RelacionamentoOneToOneTest extends EntityManagerTest {
-	
+
 	@Test
 	public void verificaRelacionamento() {
 		Pedido pedido = entityManager.find(Pedido.class, 1);
-		
+
 		PagamentoCartao pagamentoCartao = new PagamentoCartao();
 		pagamentoCartao.setNumero("1234");
 		pagamentoCartao.setStatus(StatusPagamento.PROCESSANDO);
 		pagamentoCartao.setPedido(pedido);
-		
+
 		entityManager.getTransaction().begin();
 		entityManager.persist(pagamentoCartao);
 		entityManager.getTransaction().commit();
-		
+
 		entityManager.clear();
-		
+
 		Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
 		Assertions.assertNotNull(pedidoVerificacao.getPagamento());
-		
+
 		PagamentoCartao pagamentoCartaoVerificacao = entityManager.find(PagamentoCartao.class, pagamentoCartao.getId());
 		Assertions.assertEquals("1234", pagamentoCartao.getNumero());
-		
+
 		System.out.println("----------------------------------------------------------");
 		System.out.println("Pedido id: " + pedidoVerificacao.getId() + " data: " + pedidoVerificacao.getDataPedido());
-		for(ItemPedido itemPedido:pedidoVerificacao.getItens()) {
-			System.out.println("Produto: " + itemPedido.getProduto().getNome() + "\tQuant X Valor: " + itemPedido.getQuantidade() + " x " +
-					itemPedido.getProduto().getPreco());
+		for (ItemPedido itemPedido : pedidoVerificacao.getItens()) {
+			System.out.println("Produto: " + itemPedido.getProduto().getNome() + "\tQuant X Valor: "
+					+ itemPedido.getQuantidade() + " x " + itemPedido.getProduto().getPreco());
 		}
 		System.out.println("Total: " + pedido.getTotal());
 		System.out.println("----------------------------------------------------------");
 	}
-	
+
+	@Test
+	public void verificaRelacionamentoPedidoNotaFiscal() {
+		Pedido pedido = entityManager.find(Pedido.class, 1);
+
+		NotaFiscal notaFiscal = new NotaFiscal();
+		notaFiscal.setXml("TESTE");
+		notaFiscal.setDataEmissao(new Date());
+		notaFiscal.setPedido(pedido);
+		
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(notaFiscal);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+		Assertions.assertNotNull(pedidoVerificacao.getNotaFiscal());
+
+		NotaFiscal notaFiscalVerificacao = entityManager.find(NotaFiscal.class, notaFiscal.getId());
+		Assertions.assertEquals("TESTE", notaFiscalVerificacao.getPedido().getNotaFiscal().getXml());
+
+	}
 }
